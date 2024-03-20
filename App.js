a68f9633ed1c9eee20fbd81c6d68a132bb0eadeb
tommoko3ds <kenyaescamill@gmail.com>
2024-03-20 11:46:20 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
+  const data = 'Texto a codificar en el código QR';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+<Image
+          source={require('./img/carro.png')}
+          style={styles.imagen}
+        />
+      <Text>ParkPal, tu Estacionamiento inteligente</Text>
+      <QRCode
+        value={data}
+        size={200}
+      />
+     
+       
+      </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#21196F',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  iconContainer: {
+    marginTop: 20,
+    width: 60, // Ancho del contenedor del icono
+    height: 60, // Altura del contenedor del icono
+    borderRadius: 30, // Mitad del ancho o la altura para un círculo perfecto
+    backgroundColor: 'white', // Color de fondo del contenedor del icono
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imagen: {
+    width: 180, // Ancho de la imagen
+    height: 180, // Altura de la imagen
+    borderRadius: 80, // Mitad del ancho o la altura para un círculo perfecto
   },
 });
